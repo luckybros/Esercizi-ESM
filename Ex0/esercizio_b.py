498f8de97fbdf5ca4708748e71ca314b6643a5a1
luca <luca.scolletta@outlook.it>
2024-04-23 00:01:40 +0200

    aggiunto esercizio b

---
+++ Ex0/esercizio_b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Esercizio b
+"""
+import numpy as np
+
+def clip(x):
+   for i in range (0, len(x)):
+       np.sort(x[i])
+       for j in range(0, len(x[i])-1):
+           x[i][j] = 0
+           
+A = np.array([[1,2,3,4], 
+              [5,6,7,8], 
+              [9,10,11,12], 
+              [13,14,15,16]])
+
+clip(A)
+print(A)
